a52c3d8c180848c32e66843c9d3832bae04bcf80
oscar <oscarcurso1976@gmail.com>
2018-10-01 13:59:53 +0200

    Initial commit

---
+++ Assets/_GameAssets/Scripts/Cubo.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cubo : MonoBehaviour {
+
+
+    AudioSource AudioSource;
+    
+    // Use this for initialization
+    void Start () {
+        AudioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+        {
+
+        AudioSource.Play();
+        Invoke("DestruirObjeto", 3);
+        
+        }
+        void DestruirObjeto()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/_GameAssets/Scripts/Pollo.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pollo : MonoBehaviour {
+
+
+
+    Rigidbody rb;
+    [SerializeField] int fuerza = 100;
+    [SerializeField] Transform posPies;
+
+
+    // Use this for initialization
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (Input.GetKeyDown("space"))
+
+            
+
+        {
+            Collider[] cols=Physics.OverlapSphere(posPies.position, 0.1f, LayerMask.NameToLayer("Terreno"));
+                for ( int i = 0; i <cols.Length; i++) 
+                {
+                Debug.Log("Estoy tocando con los pies en algo" +i);
+            }
+
+            rb.AddForce(new Vector3(1, 1, 0) * fuerza);
+        }
+    
+
+        else if (Input.GetKeyDown("z"))
+
+        {
+            rb.AddForce(new Vector3(1, 1, 1) * fuerza);
+        }
+
+        else if (Input.GetKeyDown("x"))
+
+        {
+            rb.AddForce(new Vector3(1, 1, -1) * fuerza);
+        }
+
+
+
+    }
+
+   
+    
+}
